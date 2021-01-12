9f92968127c002e8c5b3c95264ce228adf225d3f
xcrowpk <xcrowpk@gmail.com>
2021-01-12 17:03:24 -0300

    condicionales

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #saludo = "hola "
 #nombre = input("ingrese su nombre: ")
 #print (saludo + nombre)
+
 num1 = int(input("ingresar nro1: "))
 num2 = int(input("ingresar nro2: "))
-print(num1 + num2)
+num3 = int(input("ingresar nro2: "))
+prom = (num1 + num2 + num3) / 3
+print ("el promedio es %.2f"%prom)
+if (prom >= 6):
+	print("aprovado")
+else:
+	print("desaprobado")
+
